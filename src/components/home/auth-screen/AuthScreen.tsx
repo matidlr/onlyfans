a2e59971eca885d08c4d6409f280099e94a4d9c1
Matias <mati.delarosa1984@gmail.com>
2024-06-30 17:54:52 -0300

    toggle theme

--- src/components/home/auth-screen/AuthScreen.tsx
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -1,3 +1,4 @@
+import UnderlinedText from "@/components/decorators/UnderlinedText"
 import HeroSection from "./HeroSection"
 
 
@@ -5,6 +6,17 @@ const AuthScreen = () => {
   return (
     <div className="flex flex-col">
       <HeroSection />
+      <div className="mb-20 mt-12">
+        <div className="max-w-6xl ">
+          <p className="text-3xl md:text-5xl tracking-tight mt-4 mb-8 font-semibold text-center">
+          Today's{" "}
+						<UnderlinedText className='underline-offset-8 md:underline-offset-[12px] decoration-wavy'>
+							Highlight
+						</UnderlinedText>
+            <span className='text-2xl md:text-4xl ml-1'>👇</span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
