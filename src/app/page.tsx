85c6e230aa922c0045178ff5800c6abab73fe62b
Matias <mati.delarosa1984@gmail.com>
2024-06-30 15:02:15 -0300

    create home page and register buttons with kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import AuthScreen from "@/components/home/auth-screen/AuthScreen";
+import HomeScreen from "@/components/home/home-screen/HomeScreen";
 
 
 export default function Home() {
+  const user = false;
   return (
     <main className="">
-       <h2>hello</h2>
+       {user ? <HomeScreen /> : <AuthScreen />}
     </main>
   );
 }
---
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "./HeroSection"
+
+
+const AuthScreen = () => {
+  return (
+    <div className="flex flex-col">
+      <HeroSection />
+    </div>
+  )
+}
+
+export default AuthScreen
